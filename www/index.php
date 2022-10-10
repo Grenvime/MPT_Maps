9e5e111b32e8afc05c499c0cd4a20739c57b6a62
Grenvime <Prokaznikjealous@yandex.ru>
2022-10-10 20:40:55 +0300

    Initial Commit

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<header>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>MPT_Maps</title>
+    <script src="js/main.js"></script>
+    <link href="css_styles/main.css" rel="stylesheet" type="text/css">
+</header>
+<body>
+<div>
+
+</div>
+</body>
+<footer>
+
+</footer>
+</html>
