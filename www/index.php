15d39f990141461e31e45dfdc53507c53fda4968
Grenvime <Prokaznikjealous@yandex.ru>
2022-10-10 21:24:16 +0300

    test

--- www/index.php
+++ www/index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="ru">
-<header>
+<header title="MPT_Maps">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>MPT_Maps</title>
-    <script src="js/main.js"></script>
+    <script src="js/main.js" type="text/javascript"></script>
     <link href="css_styles/main.css" rel="stylesheet" type="text/css">
 </header>
 <body>
 <div>
-
+    <script>
+        document.write("aesasno");
+    </script>
 </div>
 </body>
 <footer>
